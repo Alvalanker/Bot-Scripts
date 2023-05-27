3fa59e93c3d48a6969852279e43f239e1d2c7932
Alvalanker Highwind <123333383+Alvalanker@users.noreply.github.com>
2023-05-27 00:15:42 -0400

    Update AlvaInfo Basic.py

--- AlvaInfo Basic.py
+++ AlvaInfo Basic.py
@@ -6,37 +6,101 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command()
+async def music(ctx):
+    response = "All info about Alvalanker: LINKS: https://linktr.ee/alvalanker"
+    await ctx.send(response)
+
+
+@bot.command()
+async def spotify(ctx):
+    response = "Spotify https://open.spotify.com/artist/12O6L9BEaefqiZ5eQ2XD9R"
+    await ctx.send(response)
+
+
+@bot.command()
+async def itunes(ctx):
+    response = "Apple Music https://music.apple.com/us/artist/alvalanker/1403509237"
+    await ctx.send(response)
+
+
+@bot.command()
+async def soundcloud(ctx):
+    response = "SoundCloud https://soundcloud.com/alvalanker/radiation"
+    await ctx.send(response)
+
+
+@bot.command()
+async def discord(ctx):
+    response = "Discord https://discord.gg/tzycU3vf6r"
+    await ctx.send(response)
+
+
+@bot.command()
+async def instagram(ctx):
+    response = "Instagram https://www.instagram.com/alvalanker1"
+    await ctx.send(response)
+
+
+@bot.command()
+async def facebook(ctx):
+    response = "Facebook http://http://www.facebook.com/relicradiationmusic"
+    await ctx.send(response)
+
+
+@bot.command()
+async def youtube(ctx):
+    response = "YouTube https://www.youtube.com/@alvalankerofficial"
+    await ctx.send(response)
+
+
+@bot.command()
+async def hello(ctx):
+    response = f"Hello {ctx.author.mention}! I am just here to provide an information database for Alvalanker. I can't really help you with anything else at this time, but I hope to be upgraded in the near future. Until then, feel free to ask Rommel to tell you a joke!"
+    await ctx.send(response)
+
+
+@bot.command()
+async def joke(ctx):
+    response = "Why don't scientists trust atoms? Because they make up everything!"
+    await ctx.send(response)
+
+
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)
     if bot.user.mentioned_in(message):
         author = message.author
         content = message.content.lower()
 
-        if any(keyword in content for keyword in ["music", "new", "links", "discog", "disc", "songs", "discography", "website", "info", "information"]):
-            response = f"{author.mention}, All info about Alvalanker: LINKS: https://linktr.ee/alvalanker\n"
-            # Send the response
+        if any(keyword in content for keyword in ["music", "new", "links", "link", "discog", "disc", "songs", "discography", "website", "info", "information"]):
+            response = f"{author.mention}, All info about Alvalanker: LINKS: https://linktr.ee/alvalanker"
             await message.channel.send(response)
         elif "spotify" in content:
-            response = f"Spotify https://open.spotify.com/artist/12O6L9BEaefqiZ5eQ2XD9R\n"
+            response = f"Spotify https://open.spotify.com/artist/12O6L9BEaefqiZ5eQ2XD9R"
             await message.channel.send(response)
         elif "itunes apple" in content:
-            response = f"Apple Music https://music.apple.com/us/artist/alvalanker/1403509237\n"
+            response = f"Apple Music https://music.apple.com/us/artist/alvalanker/1403509237"
             await message.channel.send(response)
         elif "soundcloud" in content:
-            response = f"SoundCloud https://soundcloud.com/alvalanker/radiation\n"
+            response = f"SoundCloud https://soundcloud.com/alvalanker/radiation"
             await message.channel.send(response)
         elif "discord" in content:
-            response = f"Discord https://discord.gg/tzycU3vf6r\n"
+            response = f"Discord https://discord.gg/tzycU3vf6r"
             await message.channel.send(response)
         elif "instagram" in content:
-            response = f"Instagram https://www.instagram.com/alvalanker1\n"
+            response = f"Instagram https://www.instagram.com/alvalanker1"
             await message.channel.send(response)
         elif "facebook" in content:
-            response = f"Facebook http://http://www.facebook.com/relicradiationmusic\n"
+            response = f"Facebook http://http://www.facebook.com/relicradiationmusic"
             await message.channel.send(response)
         elif "youtube" in content:
-            response = f"YouTube https://www.youtube.com/@alvalankerofficial\n"
+            response = f"YouTube https://www.youtube.com/@alvalankerofficial"
             await message.channel.send(response)
         elif any(keyword in content for keyword in ["hello", "sup", "hey", "whats up"]):
             response = f"Hello {author.mention}! I am just here to provide an information database for Alvalanker. I can't really help you with anything else at this time, but I hope to be upgraded in the near future. Until then, feel free to ask Rommel to tell you a joke!"
@@ -48,9 +112,8 @@ async def on_message(message):
             response = f"I'm sorry, {author.mention}, I didn't understand your question. I am only here to provide information about Alvalanker, so ask me something like 'music', 'links', 'songs', 'discography', 'website', or 'info'."
             await message.channel.send(response)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    await bot.process_commands(message)
+
 
 # Run the bot with your bot token
-bot.run("YOUR_BOT_TOKEN")
+bot.run("MTEwNzU3MzQ4NjkyMDEzODc2Mg.GlJzd-.44-btsjH7u-U4lIcvB6nIER2nNSwwlngsLvSN0")
